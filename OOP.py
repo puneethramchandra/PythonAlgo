c5ebd37d221c8d5d76049161919b03f61ca736d8
Puneeth Ram Chandra <puneethramchandra@gmail.com>
2020-08-14 20:59:22 +0530

    more about classes

--- OOP.py
+++ OOP.py
@@ -1,20 +1,25 @@
-class PlayerChar:#naming Convention for class
-    def __init__(self,name, age): #init is a magic method
-        self.name= name
-        self.age=age
+class PlayerChar:  #naming Convention for class
+    # Class object Attribute all objects can use this
+    membership=True
+    def __init__(self, name, age): #init is a magic method
+        if (PlayerChar.membership):
+            self.name = name
+            self.age = age
 
-    def run(self):
-       print('run')
-       return 'Done'
+    def shout(self):
+       return print(f'My name is {self.name}')
+
 
 
 
 player1 = PlayerChar('Puneeth',21)
-player2= PlayerChar("Rupa",46)
+player2 = PlayerChar("Rupa",46)
+
+
 
 print(player1) #Different memory locations suggests different objects
 print(player2)
 print(player1.name)
 print(player2.name)
 print(player2.age)
-print(player2.run())
+print(player2.shout())
